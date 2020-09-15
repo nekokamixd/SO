46a43eaf8e959e1cf16b86e759da99adb1b4384c
nekokamu <spaletcano@gmail.com>
2020-09-14 21:15:47 -0300

    Print nombres de archivos

--- main.c
+++ main.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
-#include <sys/stat.h> //mkdir
-//#include <sys/file.h>
-//#include <stdlib.h>
-//#include <sys/dir.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
 
-int main(){
-    // system("ls"); Para tirar comandos por consola
+int main(int argc, char **argv){
+    DIR* FD;
+    struct dirent* in_file;
+    FILE    *entry_file;
 
-    int make = mkdir("prueba", S_IRWXU | S_IRWXG | S_IRWXO);
-    printf("%d", make);
+    //int make = mkdir("prueba", S_IRWXU | S_IRWXG | S_IRWXO);
+
+    // Cambiar path
+    FD = opendir("/home/sofia/github/SO");
+    while ((in_file = readdir(FD))){
+
+        if (!strcmp (in_file->d_name, "."))
+            continue;
+        if (!strcmp (in_file->d_name, ".."))
+            continue;
+
+        entry_file = fopen(in_file->d_name, "r");
+        if (entry_file == NULL){
+            fprintf(stderr, "Error : Failed to open entry file - %s\n", strerror(errno));
+            return 1;
+        }
+
+        printf("%s", in_file->d_name);
+        printf("%s", "\n");
+        fclose(entry_file);
+    }
+
     return 0;
 }
