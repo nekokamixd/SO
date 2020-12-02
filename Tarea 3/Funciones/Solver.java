c7e449acfc2b7770b1cca90729647bec842e219e
nekokamu <spaletcano@gmail.com>
2020-12-02 16:10:42 -0300

    Creo que funciona

--- Tarea 3/Funciones/Solver.java
+++ Tarea 3/Funciones/Solver.java
@@ -22,48 +22,85 @@ public class Solver extends Thread {
     }
 
     public static int calcular(String[] funciones, String input, int cantidad) {
-        String regex = "[a-z]\\([a-z]\\)";
+        String regex = "[a-z]\\(([a-z])+\\)";
         String funcion = "";
-        String get_cuerpo;
-        String cuerpo = "";
+        String get_expresion;
+        String expresion = "";
         String calculo;
 
         // Buscar funcion en el arreglo
         for (int i = 0; i < cantidad; i++) {
-
             if (Boolean.compare(funciones[i].startsWith(input.substring(0, 1)), true) == 0) {
                 funcion = funciones[i];
-                get_cuerpo = funcion.substring(5, funcion.length());
-                cuerpo = cuerpo.concat(get_cuerpo);
-            } else {
-                System.out.println("Funcion: " + funciones[i] + " no empieza con " + input);
+                int igual = funcion.indexOf("=");
+                get_expresion = funcion.substring(igual+1, funcion.length());
+                expresion = expresion.concat(get_expresion);
             }
         }
 
         Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(cuerpo);
+        Matcher m = p.matcher(expresion);
 
+        System.out.println("Expresion: " + expresion);
         boolean coincidencia = m.find();
         if (Boolean.compare(coincidencia, false) == 0) {
-            char variable = funcion.charAt(2);
-            char valor = input.charAt(2);
+            // No hay mas funciones en la expresion
+            int cierre_parentesis_funcion = funcion.indexOf(")");
+            int cierre_parentesis_input = input.indexOf(")");
+            String variable = funcion.substring(2, cierre_parentesis_funcion);
+            String valor = input.substring(2, cierre_parentesis_input);
 
-            calculo = cuerpo.replace(variable, valor);
-            System.out.println(calculo);
+            calculo = expresion.replaceFirst(variable, valor);
+            System.out.println("Calculo: " + calculo);
 
             ScriptEngineManager mgr = new ScriptEngineManager();
             ScriptEngine engine = mgr.getEngineByName("JavaScript");
 
             try{
-                System.out.println(engine.eval(calculo));
+                int resultado = (int) engine.eval(calculo);
+                System.out.println(resultado);
+                return(resultado);
             }
             catch(ScriptException e){
-                System.out.println("xd");
+                e.printStackTrace();
+                return(0);
             }
-            return (1);
-        } else {
-            // Crear ramitas
 
+        } else {
+            // Buscar funciones en la expresion
+            for (int i = 0; i < cantidad; i++) {
+                if (expresion.indexOf(funciones[i].substring(0, 3)) > -1) {
+                    System.out.println("Estoy revisando: " + funciones[i]);
+                    int cierre_parentesis_funcion = funciones[i].indexOf(")");
+                    int cierre_parentesis_input = input.indexOf(")");
+
+                    String variable = funciones[i].substring(2, cierre_parentesis_funcion);
+                    String valor = input.substring(2, cierre_parentesis_input);
+
+                    String nueva_funcion = funciones[i].substring(0, cierre_parentesis_input+1);
+                    System.out.println(nueva_funcion);
+
+                    nueva_funcion = nueva_funcion.replace(variable, valor);
+                    int nuevo_resultado = calcular(funciones, nueva_funcion, cantidad);
+                    System.out.println("Nuevo Resultado: " + nuevo_resultado + " Funcion a reemplazar: " + funciones[i].substring(0, 4) + " Nueva funcion: " + nueva_funcion);
+
+                    System.out.println("Expresion antes del replace: " + expresion);
+                    expresion = expresion.replace(funciones[i].substring(0,4), Integer.toString(nuevo_resultado));
+                    System.out.println("Nueva luego del replace: " + expresion);
+                }
+            }
+            System.out.println("Expresion final: " + expresion);
+            ScriptEngineManager mgr = new ScriptEngineManager();
+            ScriptEngine engine = mgr.getEngineByName("JavaScript");
+
+            try{
+                int resultado_final = (int) engine.eval(expresion);
+                System.out.println(resultado_final);
+                return(resultado_final);
+            }
+            catch(ScriptException e){
+                e.printStackTrace();
+            }
         }
         return (1);
     }
