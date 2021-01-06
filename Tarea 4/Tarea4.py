73da8ded4dcb880218ca220c273b0af9df8a7da9
Sofia Palet <sofia.palet@sansano.usm.cl>
2021-01-06 18:03:13 -0300

    Al parecer hasta aqui esta funcionando

--- Tarea 4/Tarea4.py
+++ Tarea 4/Tarea4.py
@@ -1,135 +1,190 @@
-import time
 import threading
+from threading import Thread
+from threading import Semaphore
+import time
+from datetime import datetime
 
-class mesa:
+cant_bandejas = input("Cantidad de bandejas: ")
+cant_clientes = input("Cantidad de clientes: ")
+bandejas = [] # Lista de bandejas
+existeClienteAyudando = 0
+
+# Semaforo para que solo una persona use el recurso
+s_usarBandejeroFila = Semaphore(1)
+s_usarBandejero = Semaphore(1)
+s_usarMesa = Semaphore(1)
+s_JuanSirviendo = Semaphore(0)
+s_clienteAyudando = Semaphore(1)
+
+# Semaforo con la cantidad de bandejas que hay en cada bandejero
+s_cantidadBandejasFila = Semaphore(int(cant_bandejas))
+s_cantidadBandejasBandejero = Semaphore(0)
+
+"""
+Nombre: mesaAuxiliar
+Parametros: ninguno
+Descripcion: Simula al cliente que ayuda a otro cliente a comer, siendo la mesa auxiliar sus manos que mantienen la bandeja.
+"""
+class mesaAuxiliar:
     def __init__(self):
         self.bandeja = None
+        self.cliente = None
     
-    def insert_bandeja(self,bandejaEntrante):
-        self.bandeja = bandejaEntrante
+    def sostener_bandeja(self, b, c):
+        self.bandeja = b
+        self.cliente = c
 
     def release_bandeja(self):
-        aux = self.bandeja
         self.bandeja = None
-        return aux
 
     def get_bandeja(self):
         return self.bandeja
+    
+    def get_idCliente(self):
+        return self.cliente
 
+"""
+Nombre: bandeja
+Parametros: ninguno
+Descripcion: Bandeja en donde se pondrá el almuerzo.
+"""
 class bandeja:
-    def __init__(self, numero):
+    def __init__(self):
         self.comida = False
-        self.id = numero
+
+    def hayComida(self):
+        return self.comida
 
     def comer(self):
         self.comida = False
 
     def llenar(self):
-        time.sleep(3)
         self.comida = True
 
-    def getId(self):
-        return self.id
+"""
+Nombre: cocina
+Parametros: ninguno
+Descripcion: Cocina es donde se dejará la bandeja para que Juan sirva el almuerzo.
+"""
+class cocina:
+    def __init__(self):
+        self.bandeja = None
+        self.cliente = None
+    
+    def insert_bandeja(self, b, c):
+        self.bandeja = b
+        self.cliente = c
 
-class Juan(Thread):
-    def __init__(self, lock_fila, cv_fila_aCliente, cv_fila_aJuan, bandejas, servidos_actual, cocina):
-        Thread.__init__(self)
-        self.lock_fila = lock_fila
-        self.cv_fila_aCliente = cv_fila_aCliente
-        self.cv_fila_aJuan = cv_fila_aJuan
-        self.bandejas = bandejas
-        self.servidos_actual = servidos_actual
-        self.cocina = cocina
+    def release_bandeja(self):
+        self.bandeja = None
 
-    def run(self):
-        while (servidos_actual[0] < numero_atendiendo):
-        lock_fila.acquire()
-        while (cocina.get_bandeja() == None):
-            cv_fila_aJuan.wait()
-        aux_bandeja = mesa.get_bandeja()
-        aux_bandeja.llenar()
-        time.sleep(3)
-        mesa.release_bandeja()
-        servidos_actual[0] += 1
-        cv_fila_aCliente.notify_all()
-        lock_fila.release()
+    def get_bandeja(self):
+        return self.bandeja
+    
+    def get_idCliente(self):
+        return self.cliente
 
+mesa = cocina()
 
+"""
+Nombre: Cliente
+Parametros: ninguno
+Descripcion: Cliente que quiere almorzar.
+"""
 class Cliente(Thread):
-    def __init__(self, numero_atendiendo, bandeja, lock_fila, cv_fila_aCliente, cv_fila_aJuan, servidos_actual, cocina):
+    def __init__(self):
         Thread.__init__(self)
-        self.numero_atendiendo = numero_atendiendo
-        self.bandeja = bandeja
-        self.lock_fila = lock_fila
-        self.cv_fila_aCliente = cv_fila_aCliente
-        self.cv_fila_aJuan = self.cv_fila_aJuan
-        self.servidos_actual = servidos_actual
-        self.cocina = cocina
 
     def run(self):
-        print(numero_atendiendo)
-        lock_fila.acquire()
-        while (cocina.get_bandeja() != None):
-            cv_fila_aCliente.wait()
-        mesa.insert_bandeja(bandeja)
-        print("Ya comi uwu")
-        servidos_actual[1] += 1
-        cv_fila_aJuan.notify()
-        lock_fila.release()
+        global s_usarBandejeroFila, s_cantidadBandejasFila, s_cantidadBandejasBandejero
+        global bandejas, mesa, existeClienteAyudando
+        id_cliente = threading.get_ident()
 
+        # SACAR BANDEJAS
 
-def juan_funcion(lock_fila, cv_fila_aCliente, cv_fila_aJuan, bandejas, servidos_actual, cocina):
-    while (servidos_actual[0] < numero_atendiendo):
-        lock_fila.acquire()
-        while (cocina.get_bandeja() == None):
-            cv_fila_aJuan.wait()
-        aux_bandeja = mesa.get_bandeja()
-        aux_bandeja.llenar()
-        time.sleep(3)
-        mesa.release_bandeja()
-        servidos_actual[0] += 1
-        cv_fila_aCliente.notify_all()
-        lock_fila.release()
+        s_usarBandejeroFila.acquire()
+        now = datetime.now()
+        f_clientes = open("clientes.txt", "a")
+        f_clientes.write("Cliente " + str(id_cliente) + " sacando bandeja en la fila, hora: " + now.strftime("%H:%M:%S") + "\n")
+        f_clientes.close()
+        bandeja = bandejas.pop(0)
+        s_cantidadBandejasFila.acquire()
+        s_usarBandejeroFila.release()
 
-def cliente_funcion(numero_atendiendo, bandeja, lock_fila, cv_fila_aJuan, cv_fila_aCliente, servidos_actual, cocina):
-    print(numero_atendiendo)
-    lock_fila.acquire()
-    while (cocina.get_bandeja() != None):
-        cv_fila_aCliente.wait()
-    mesa.insert_bandeja(bandeja)
-    print("Ya comi uwu")
-    servidos_actual[1] += 1
-    cv_fila_aJuan.notify()
-    lock_fila.release()
+        # BUSCAR ALMUERZO
 
-lock_fila= threading.Lock()
-cv_fila_aJuan = threading.Condition(lock_fila)
-cv_fila_aCliente = threading.Condition(lock_fila)
+        s_usarMesa.acquire()
+        now = datetime.now()
+        f_clientes = open("clientes.txt", "a")
+        f_clientes.write("Cliente " + str(id_cliente) + " esperando a que le Juan le sirva almuerzo, hora: " + now.strftime("%H:%M:%S") + "\n")
+        f_clientes.close()
+        mesa.insert_bandeja(bandeja, id_cliente)
+        s_JuanSirviendo.acquire()
+        s_usarMesa.release()
 
-servidos_actual = [0, 0]
+        # COMER ALMUERZO
+        #if ():
+        #else:
 
-cant_bandejas = input("Cantidad de bandejas: ")
-cant_clientes = input("Cantidad de clientes: ")
+        time.sleep(5) # Cliente come su almuerzo
+        s_cantidadBandejasBandejero.release()
+        now = datetime.now()
+        f_clientes = open("clientes.txt", "a")
+        f_clientes.write("Cliente " + str(id_cliente) + " termino de comer, hora: " + now.strftime("%H:%M:%S") + "\n")
+        f_clientes.close()
 
-bandejaId = 0
-bandejas = []
-for b in range(int(cant_bandejas)):
-    bandejas.append(bandeja(b))
+        # AYUDAR AL SIGUIENTE
 
-cocina = mesa()
+        # DEJAR BANDEJA
 
-numero_atendiendo = 1
+"""
+Nombre: Cliente
+Parametros: ninguno
+Descripcion: Juan, el encargado de servir almuerzos.
+"""
+class Juan(Thread):
+    def __init__(self):
+        Thread.__init__(self)
 
-f1 = open("juan.txt", "w")
-f2 = open("clientes.txt", "w")
+    def run(self):
+        global s_JuanSirviendo
+        global mesa
+        sirviendo_almuerzos = True
 
-thread_juan = threading.Thread(name="hilo"+"Juan", target=juan_funcion, args=(lock_fila, cv_fila_aCliente, cv_fila_aJuan, bandejas, servidos_actual, cocina))
-thread_juan.start()
+        while (sirviendo_almuerzos):
+        
+            # SERVIR ALMUERZO
+            
+            if (mesa.get_bandeja() != None):
+                cliente_servir = mesa.get_idCliente()
+                f_juan = open("juan.txt", "a")
+                now = datetime.now()
+                f_juan.write("Sirviendo al cliente " + str(cliente_servir) + ", hora: " + now.strftime("%H:%M:%S") + "\n")
+                f_juan.close()
+                time.sleep(3) # Juan sirve almuerzo
+                mesa.get_bandeja().llenar()
+                s_JuanSirviendo.release()
+                mesa.release_bandeja()
 
-for cliente in range(int(cant_clientes)):
-    thread = threading.Thread(name="hilo"+str(cliente), target=cliente_funcion, args=(numero_atendiendo, bandejas.pop(0), lock_fila, cv_fila_aJuan, cv_fila_aCliente, servidos_actual, cocina))
-    thread.start()
-    numero_atendiendo += 1
+            # RELLENAR BANDEJEROS
 
-f1.close()
-f2.close()
+def main(): 
+    global cant_bandejas, cant_clientes, bandejas, mesa
+
+    for b in range(int(cant_bandejas)):
+        bandejas.append(bandeja())
+
+    f_Juan = open("juan.txt", "w")
+    f_Clientes = open("clientes.txt", "w")
+    f_Juan.close()
+    f_Clientes.close()
+
+    thread_juan = Juan()
+    thread_juan.start()
+    
+    for cliente in range(int(cant_clientes)):
+        thread = Cliente()
+        thread.start()
+   
+if __name__ == '__main__':
+    main()
